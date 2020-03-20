c1248dfeadb6590be9ae577bd2e11eba83e95211
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-20 22:21:04 +0000

    Done.

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -34,14 +34,18 @@ class Artist
   end
 
   def add_song(song)
-    if song.artist == self
+    if song.artist == self #checks to see if song has an artist. If not, assigns artist to song 
     else
       song.artist = self
-      @songs << song
     end
+
+    songs << song unless songs.include?(song) #checks to see if song exist, then pushes the song to the array.
+
   end
 
   def genres
-    self.songs.collect {|artist| artist.genre}.uniq
+
+    # binding.pry
+    self.songs.collect {|artist| artist.genre}.uniq #goes over the songs method and pulls the artists
   end
 end
