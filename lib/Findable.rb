cfa002d831eddb464fb0dbd5f753b2207be2a692
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-20 23:09:47 +0000

    Done.

---
+++ lib/Findable.rb
@@ -0,0 +1,13 @@
+module Concerns::Findable
+
+  def find_by_name(name) #this works like a generic find_by_name method that was introduced in the Song class
+    all.find do |object|
+        object.name == name
+    end
+  end
+
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
+end
